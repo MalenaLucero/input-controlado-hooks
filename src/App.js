f7887be2ea1fd2a6f8e15111d9dec45eb33c44ef
MalenaLucero <luceromalena@hotmail.com>
2019-11-27 14:26:37 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ function App() {
   const [Objeto, setObjeto] = useState(ObjetoModelo)
   return (
     <div>
-      <input placeholder={'Origen'} value={Objeto.origen} onChange={(e)=>setObjeto({...Objeto, origen: e.target.value})}></input>
-      <input placeholder={'Destino'} value={Objeto.destino} onChange={(e)=>setObjeto({...Objeto, destino: e.target.value})}></input>
+      <input 
+        placeholder={'Origen'} 
+        value={Objeto.origen} 
+        onChange={(e)=>setObjeto({...Objeto, origen: e.target.value})}></input>
+      <input 
+        placeholder={'Destino'} 
+        value={Objeto.destino} 
+        onChange={(e)=>setObjeto({...Objeto, destino: e.target.value})}></input>
       <button onClick={()=>{console.log(Objeto)}}>Confirmar</button>
     </div>
   );
