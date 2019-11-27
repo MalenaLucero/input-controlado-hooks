e87fb53a8e1e573b6cc2e1541fb60d379cb5afd3
MalenaLucero <luceromalena@hotmail.com>
2019-11-27 14:12:16 -0300

    dos inputs controlados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from  'react'
+
+const ObjetoModelo = {
+  origen: '',
+  destino: ''
+}
 
 function App() {
+  const [Objeto, setObjeto] = useState(ObjetoModelo)
   return (
     <div>
-      Hola
+      <input placeholder={'Origen'} value={Objeto.origen} onChange={(e)=>setObjeto({...Objeto, origen: e.target.value})}></input>
+      <input placeholder={'Destino'} value={Objeto.destino} onChange={(e)=>setObjeto({...Objeto, destino: e.target.value})}></input>
+      <button onClick={()=>{console.log(Objeto)}}>Confirmar</button>
     </div>
   );
 }
